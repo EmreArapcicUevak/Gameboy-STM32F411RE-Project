db6ca9fed84658c598a54235b55e72786f22d4aa
Emre ArapcicUevak <90074488+EmreArapcicUevak@users.noreply.github.com>
2025-05-02 20:49:54 +0200

    Add some code to show a Swiss Flag on the display 🇨🇭

--- src/main.c
+++ src/main.c
@@ -16,6 +16,28 @@ int main(){
 
   uart2_println("Programm started");
   write_command(0x29);
+  set_column_address(0,239); 
+  set_page_address(0,319);
+  start_memory_write();
+
+  for (unsigned int i = 0; i < 240*320; i++)
+    send_multiple_data((uint8_t []) {0xF8, 0x00}, 2);
+
+  set_column_address(239/2 - 10, 239/2 + 10);
+  set_page_address(319/2 - 50,319/2 + 50);
+  start_memory_write();
+  for (unsigned int i = 0; i < 320*20; i++)
+    send_multiple_data((uint8_t []) {0xFF, 0xFF}, 2); 
+
+
+  set_column_address(239/2 - 50, 239/2 + 50);
+  set_page_address(319/2 - 10,319/2 + 10);
+  start_memory_write();
+
+  for (unsigned int i = 0; i < 320*20; i++)
+    send_multiple_data((uint8_t []) {0xFF, 0xFF}, 2); 
+  uart2_println("--------");
+
   while (1) {
     GPIOA->ODR ^= GPIO_ODR_OD5;        // Toggle LED on PA5
     wait(500);
