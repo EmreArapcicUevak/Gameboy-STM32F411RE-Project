127e18f52ee362b76137443dc00124136b3c33df
Emre ArapcicUevak <90074488+EmreArapcicUevak@users.noreply.github.com>
2025-05-02 20:49:28 +0200

    Add more functions to the init of the display and add functons for setting the page and collumn address

--- lib/Display/src/Display.c
+++ lib/Display/src/Display.c
@@ -22,13 +22,16 @@ void init_display(void) {
   GPIOB->AFR[0] |= (2 << GPIO_AFRL_AFSEL0_Pos); 
   GPIOB->OSPEEDR |= (0b10 << GPIO_OSPEEDR_OSPEED0_Pos); // PB0 Fast Speed output
 
+  GPIOC->ODR &= ~GPIO_ODR_OD0; // Keep display reset pin on low
+  wait(10);
   GPIOC->ODR |= GPIO_ODR_OD0; // Keep display reset pin on low
+  wait(10);
   
   SPI2->CR1 |= SPI_CR1_SSM | SPI_CR1_MSTR | SPI_CR1_SSI; // Manager slave or master mode behaviour via software
   // Leave clock polarity 0 and clock phase 0 
 
-  NVIC->ISER[1] |= (0b1 << 4); // enable interrupt for SPI2
-  SPI2->CR2 |= SPI_CR2_RXNEIE;
+  //NVIC->ISER[1] |= (0b1 << 4); // enable interrupt for SPI2
+  //SPI2->CR2 |= SPI_CR2_RXNEIE;
 
 
   TIM3->CR1 |= TIM_CR1_ARPE | TIM_CR1_URS;
@@ -41,6 +44,47 @@ void init_display(void) {
   TIM3->CR1 |= TIM_CR1_CEN; // Turn on the timer
 
   SPI2->CR1 |= SPI_CR1_SPE; // Turn on the SPI
+
+  write_command(0x01); // Software reset
+  wait(5);
+
+  write_command(0x28); // Display OFF
+
+  write_command(0xCF); send_data(0x00); send_data(0x83); send_data(0x30);
+  write_command(0xED); send_data(0x64); send_data(0x03); send_data(0x12); send_data(0x81);
+  write_command(0xE8); send_data(0x85); send_data(0x01); send_data(0x79);
+
+  write_command(0xCB); send_data(0x39); send_data(0x2C); send_data(0x00); send_data(0x34); send_data(0x02);
+  write_command(0xF7); send_data(0x20);
+  write_command(0xEA); send_data(0x00); send_data(0x00);
+
+  write_command(0xC0); send_data(0x26);         // Power control 1
+  write_command(0xC1); send_data(0x11);         // Power control 2
+  write_command(0xC5); send_data(0x35); send_data(0x3E); // VCOM control
+  write_command(0xC7); send_data(0xBE);
+
+  write_command(0x36); send_data(0x48);         // Memory access control
+  write_command(0x3A); send_data(0x55);         // Pixel format = 16 bit
+
+  write_command(0xB1); send_data(0x00); send_data(0x1B); // Frame rate
+  write_command(0xF2); send_data(0x08);         // 3Gamma Function Disable
+  write_command(0x26); send_data(0x01);         // Gamma curve selected
+  write_command(0xE0); // Positive gamma correction
+  send_data(0x1F); send_data(0x1A); send_data(0x18); send_data(0x0A);
+  send_data(0x0F); send_data(0x06); send_data(0x45); send_data(0x87);
+  send_data(0x32); send_data(0x0A); send_data(0x07); send_data(0x02);
+  send_data(0x07); send_data(0x05); send_data(0x00);
+
+  write_command(0xE1); // Negative gamma correction
+  send_data(0x00); send_data(0x25); send_data(0x27); send_data(0x05);
+  send_data(0x10); send_data(0x09); send_data(0x3A); send_data(0x78);
+  send_data(0x4D); send_data(0x05); send_data(0x18); send_data(0x0D);
+  send_data(0x38); send_data(0x3A); send_data(0x1F);
+
+  write_command(0x11); // Sleep OUT
+  wait(100);
+
+  write_command(0x29); // Display ON
 }
 
 
@@ -48,18 +92,24 @@ void write_command(uint8_t command) {
   GPIOC->ODR &= ~(GPIO_ODR_OD1);
   while (!(SPI2->SR & SPI_SR_TXE)); // Wait until transmit buffer empty
   SPI2->DR = command;
+  while (!(SPI2->SR & SPI_SR_TXE)); // Wait until transmit register is empty
+  while (!(SPI2->SR & SPI_SR_RXNE)); // Wait until something has been received
+  volatile uint8_t dummy = SPI2->DR; // Read and discard received byte
 }
 
 void send_data(uint8_t data) {
   GPIOC->ODR |= (GPIO_ODR_OD1);
   while (!(SPI2->SR & SPI_SR_TXE));
   SPI2->DR = data;
+  while (!(SPI2->SR & SPI_SR_TXE)); // Wait until transmit register is empty
+  while (!(SPI2->SR & SPI_SR_RXNE)); // Wait until something has been received
 }
 
 void send_multiple_data(uint8_t *data_arr, const unsigned int size) {
   for (uint8_t *data = data_arr; data < data_arr + size; data++)
     send_data(*data);
 }
+
 
 void SPI2_IRQHandler(void) {
   if (SPI2->SR & SPI_SR_TXE) {
--- lib/Display/src/Display.h
+++ lib/Display/src/Display.h
@@ -8,5 +8,31 @@ void write_command(uint8_t command); // Writes a command to the display
 void send_data(uint8_t data); // sends data to the display
 void send_multiple_data(uint8_t *data_arr, const unsigned int size); // send multiple data to the display
 
+static inline void set_column_address(uint16_t x0, uint16_t x1) {
+  write_command(0x2A);
+  uint8_t data[] = {
+      (x0 >> 8) & 0xFF, x0 & 0xFF,
+      (x1 >> 8) & 0xFF, x1 & 0xFF
+  };
+  send_multiple_data(data, 4);
+}
 
+static inline void set_page_address(uint16_t y0, uint16_t y1) {
+  write_command(0x2B);
+  uint8_t data[] = {
+      (y0 >> 8) & 0xFF, y0 & 0xFF,
+      (y1 >> 8) & 0xFF, y1 & 0xFF
+  };
+  send_multiple_data(data, 4);
+}
+
+static inline void start_memory_write(void) {
+  write_command(0x2C);
+}
+
+static inline void set_address_window(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1) {
+  set_column_address(x0, x1);
+  set_page_address(y0, y1);
+  start_memory_write();
+}
 #endif
