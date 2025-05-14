344ec60b917f4b149a0112060a05d8ff79e99618
Emre ArapcicUevak <90074488+EmreArapcicUevak@users.noreply.github.com>
2025-05-14 22:09:16 +0200

    Make the functions static in Color.h

--- include/Color.h
+++ include/Color.h
@@ -1,5 +1,6 @@
-#ifndef my_Color
-#define my_Color
+#ifndef MY_COLOR
+#define MY_COLOR
+#define COLOR(r, g, b) ((Color){ .red = r, .green = g, .blue = b })
 
 #include "stm32f411xe.h"
 
@@ -7,7 +8,7 @@ typedef struct {
   uint8_t red, green, blue;  
 } Color;
 
-Color lerp_color(const Color color1, const Color color2, float t) { // lerp from Color 1 to Color 2
+static Color lerp_color(const Color color1, const Color color2, float t) { // lerp from Color 1 to Color 2
   Color result;
   result.red = color1.red + (color2.red - color1.red) * t;
   result.green = color1.green + (color2.green - color1.green) * t;
