c3cbc403b14f2fba4f840ddb2cdd50a63608416d
Emre ArapcicUevak <90074488+EmreArapcicUevak@users.noreply.github.com>
2025-05-14 23:04:09 +0200

    Make the battery ADC now trigger once every 10 seconds so it doesnt spam the main programm loop

--- lib/Battery/src/Battery.c
+++ lib/Battery/src/Battery.c
@@ -3,10 +3,11 @@
 
 // PA0 is our ADC input ADC1_0
 void init_battery_ADC(uint16_t *ADCAddress, uint16_t *backupAddress) {
+  RCC->APB1ENR |= RCC_APB1ENR_TIM5EN;
   RCC->APB2ENR |= RCC_APB2ENR_ADC1EN;
   RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN | RCC_AHB1ENR_DMA2EN;
 
-  ADC1->CR2 |= ADC_CR2_ADON | ADC_CR2_CONT | ADC_CR2_DMA | ADC_CR2_DDS; // Turn on the ADC whilst we set everything else up to allow it to stabilize 
+  ADC1->CR2 |= ADC_CR2_ADON | ADC_CR2_DMA | ADC_CR2_DDS ; // Turn on the ADC whilst we set everything else up to allow it to stabilize 
   ADC1->SMPR2 |= (0b111 << ADC_SMPR2_SMP0_Pos);
 
   // ADC1_0 is the default and 1 scan is also the default so no extra setting up needed
@@ -23,7 +24,23 @@ void init_battery_ADC(uint16_t *ADCAddress, uint16_t *backupAddress) {
   NVIC_EnableIRQ(DMA2_Stream0_IRQn); // Enable the interrupts for the DMA2 Stream 0
 
   DMA2_Stream0->CR |= DMA_SxCR_EN; // Enable the stream
-  ADC1->CR2 |= ADC_CR2_SWSTART;
+  
+  ADC1->CR2 |= ADC_CR2_SWSTART; // Start the initial read via software
+  while (!(ADC1->SR & ADC_SR_EOC));
+
+  ADC1->CR2 |= (0b01 << ADC_CR2_EXTEN_Pos) | (0b1010 << ADC_CR2_EXTSEL_Pos); // ADC triggers conversion on rising edge for Timer 5 CC1
+
+  TIM5->CR1 |= TIM_CR1_ARPE;
+
+  TIM5->PSC = 15999; // from 16MHz to 1KHz meaning that every tick is 1ms
+  TIM5->ARR = 9999;
+  TIM5->CCR1 = 9999;
+
+  TIM5->CCMR1 |= TIM_CCMR1_OC1PE | (0b111 << TIM_CCMR1_OC1M_Pos);
+  TIM5->CCER |= TIM_CCER_CC1E;
+
+  TIM5->EGR |= TIM_EGR_UG;
+  TIM5->CR1 |= TIM_CR1_CEN;
 }
 
 uint16_t read_battery_ADC(void) {
@@ -37,12 +54,21 @@ float get_battery_voltage(void) {
   return ((float)read_battery_ADC() / 4095.0f) * 3.3;
 }
 
+#include <stdio.h>
+#include <stdint.h>
+
+
 void DMA2_Stream0_IRQHandler(void) {
   if (DMA2->LISR & DMA_LISR_TCIF0_Msk){
+    float voltage = get_battery_voltage(); // Convert to volts
+
+    uint32_t whole = (uint32_t)voltage;              // Integer part
+    uint32_t fraction = (uint32_t)((voltage - whole) * 1000); // Decimal part
+
+    char buffer[32];
+    snprintf(buffer, sizeof(buffer), "Voltage: %lu.%03luV", whole, fraction);
+    uart2_println(buffer);
     DMA2->LIFCR |= DMA_LIFCR_CTCIF0;
-    //uart2_println("Battery ADC value is:");
-    //uart2_send_bin_num(read_battery_ADC, 16);
-    //uart2_println("");
   }
 
   // More code here
